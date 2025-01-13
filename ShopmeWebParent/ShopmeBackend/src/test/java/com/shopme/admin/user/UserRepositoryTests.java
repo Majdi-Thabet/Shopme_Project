656489841d067744ec347b642f853987df68265e
Mejdi Thabet <majdi.thabet1997@gmail.com>
2025-01-13 16:36:40 +0100

    display user informations in front end

--- ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -2,7 +2,6 @@ package com.shopme.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -95,7 +94,6 @@ public class UserRepositoryTests {
 		repo.deleteById(userId);
 	}
 	
-		
-		
+	
 	
 }

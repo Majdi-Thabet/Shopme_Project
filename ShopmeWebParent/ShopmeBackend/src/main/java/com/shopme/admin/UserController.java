4de8959faae285e62b5520c6548c2e1b0e6a2eaa
Mejdi Thabet <majdi.thabet1997@gmail.com>
2025-01-14 16:19:01 +0100

    saving a new User after filling the Form

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/UserController.java
@@ -7,9 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
+
+/* Le Controller gère les requêtes HTTP et renvoie des vues (comme des fichiers HTML ou des templates).
+ * 
+ * Le contrôleur appelle la méthode listAll() du service, qui utilise le repository pour obtenir tous les utilisateurs de la base de données.
+ * 
+ * La liste des utilisateurs est ajoutée au modèle sous le nom "listUsers"
+ * */
 @Controller
 public class UserController {
 	
@@ -21,5 +30,25 @@ public class UserController {
 		List<User> listUsers = service.listAll();
 		model.addAttribute("listUsers", listUsers);
 		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		
+		
+		User user = new User();
+		user.setEnabled(true);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user) {
+		System.out.println(user);
+		service.save(user);
+		return "redirect:/users";
 	}
 }
